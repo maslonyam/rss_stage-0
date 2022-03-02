263e290400a24269cc5e3322af151afd285219f1
maslonyam <maslonyam@gmail.com>
2022-03-02 17:53:31 +0700

    feat: add artistBase object

---
+++ audio-player/artistsBase.js
@@ -0,0 +1,208 @@
+const artists = {
+  "awfultune": {
+    "name": "awfultune",
+    "albums": {
+      "album_1": {
+        "name": "redesign",
+        "cover": "./assets/img/awfultune/album_1.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/awfultune/album_1/track_1.mp3",
+            "header": "redesign",
+            "time": "2:28"
+          }
+        }
+      },
+      "album_2": {
+        "name": "I Met Sarah in the Bathroom",
+        "cover": "./assets/img/awfultune/album_2.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/awfultune/album_2/track_1.mp3",
+            "header": "I Met Sarah in the Bathroom",
+            "time": "2:01"
+          }
+        }
+      }
+    }
+  },
+  "twenty_one_pilots": {
+    "name": "twenty one pilots",
+    "albums": {
+      "album_1": {
+        "name": "trench",
+        "cover": "./assets/img/twenty_one_pilots/album_1.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/twenty_one_pilots/album_1/track_1.mp3",
+            "header": "jumpsuit",
+            "time": "3:58"
+          },
+          "track_2": {
+            "directory": "./assets/audio/twenty_one_pilots/album_1/track_2.mp3",
+            "header": "nico and the niners",
+            "time": "3:45"
+          }
+        }
+      },
+      "album_2": {
+        "name": "scaled and icy",
+        "cover": "./assets/img/twenty_one_pilots/album_2.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/twenty_one_pilots/album_2/track_1.mp3",
+            "header": "the outside",
+            "time": "3:36"
+          },
+          "track_2": {
+            "directory": "./assets/audio/twenty_one_pilots/album_2/track_2.mp3",
+            "header": "shy away",
+            "time": "2:55"
+          }
+        }
+      }
+    }
+  },
+  "milky_chance": {
+    "name": "Milky Chance",
+    "albums": {
+      "album_1": {
+        "name": "Sadnecessary",
+        "cover": "./assets/img/milky_chance/album_1.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/milky_chance/album_1/track_1.mp3",
+            "header": "Stolen Dance",
+            "time": "5:13"
+          },
+          "track_2": {
+            "directory": "./assets/audio/milky_chance/album_1/track_2.mp3",
+            "header": "Fairytale",
+            "time": "4:16"
+          }
+        }
+      },
+      "album_2": {
+        "name": "Blossom",
+        "cover": "./assets/img/milky_chance/album_2.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/milky_chance/album_2/track_1.mp3",
+            "header": "Blossom",
+            "time": "4:14"
+          }
+        }
+      }
+    }
+  },
+  "dennis_lloyd": {
+    "name": "Dennis Lloyd",
+    "albums": {
+      "album_1": {
+        "name": "Leftlovers",
+        "cover": "./assets/img/dennis_lloyd/album_1.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/dennis_lloyd/album_1/track_1.mp3",
+            "header": "Leftlovers",
+            "time": "3:12"
+          }
+        }
+      },
+      "album_2": {
+        "name": "Nevermind",
+        "cover": "./assets/img/dennis_lloyd/album_2.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/dennis_lloyd/album_2/track_1.mp3",
+            "header": "Nevermind",
+            "time": "2:37"
+          }
+        }
+      },
+      "album_3": {
+        "name": "Snow White",
+        "cover": "./assets/img/dennis_lloyd/album_3.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/dennis_lloyd/album_3/track_1.mp3",
+            "header": "Snow White",
+            "time": "3:15"
+          }
+        }
+      }
+    }
+  },
+  "eden": {
+    "name": "EDEN",
+    "albums": {
+      "album_1": {
+        "name": "i think you think too mush of me",
+        "cover": "./assets/img/eden/album_1.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/eden/album_1/track_1.mp3",
+            "header": "drugs",
+            "time": "5:38"
+          },
+          "track_2": {
+            "directory": "./assets/audio/eden/album_1/track_2.mp3",
+            "header": "rock + roll",
+            "time": "4:56"
+          }
+        }
+      },
+      "album_2": {
+        "name": "no future",
+        "cover": "./assets/img/eden/album_2.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/eden/album_2/track_1.mp3",
+            "header": "herts",
+            "time": "3:18"
+          }
+        }
+      }
+    }
+  },
+  "men_i_trust": {
+    "name": "Men I Trust",
+    "albums": {
+      "album_1": {
+        "name": "Show Me How",
+        "cover": "./assets/img/men_i_trust/album_1.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/men_i_trust/album_1/track_1.mp3",
+            "header": "Show Me How",
+            "time": "4:06"
+          }
+        }
+      },
+      "album_2": {
+        "name": "Numb",
+        "cover": "./assets/img/men_i_trust/album_2.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/men_i_trust/album_2/track_1.mp3",
+            "header": "Numb",
+            "time": "3:39"
+          }
+        }
+      },
+      "album_3": {
+        "name": "Lauren",
+        "cover": "./assets/img/men_i_trust/album_3.jpg",
+        "tracks": {
+          "track_1": {
+            "directory": "./assets/audio/men_i_trust/album_3/track_1.mp3",
+            "header": "Lauren",
+            "time": "3:38"
+          }
+        }
+      }
+    }
+  }
+}
+
+export default artists;
